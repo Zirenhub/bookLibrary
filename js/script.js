6bbd82fcd3690392a6e671b200e92e7a9c5c7b9c
Erdinch <erdinchorhan02@gmail.com>
2022-08-05 15:50:29 +0300

    add book popup function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
+const openModalButton = document.querySelectorAll(
+  '[data-modal-target]'
+);
+const overlay = document.getElementById('overlay');
+
+openModalButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
 let myLibrary = [];
 
 function Book() {
   // the constructor...
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
+function addBookToLibrary() {}
